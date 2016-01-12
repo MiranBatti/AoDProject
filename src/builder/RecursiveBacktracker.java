410a01644f1ea5f2c9412e251152396fd4965e86
MiranBatti <miran.batti@gmail.com>
2016-01-12 18:29:35 +0100

    up

--- src/builder/RecursiveBacktracker.java
+++ src/builder/RecursiveBacktracker.java
@@ -1,106 +1,169 @@
 package builder;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Random;
 import java.util.Stack;
 
 public class RecursiveBacktracker {
-	private final int mazeWidth, mazeHeight;
-//	private final Cell[][] mazeGrid;
+	private int width, height;
+	private ArrayList<Cell> cells; 
+	private ArrayList<Cell> paths;
+	private ArrayList<Boolean> visited;
+	private Stack<Cell> stack;
 	private Random rnd;
-	private Stack<StupidCell> stack;
-	private ArrayList<StupidCell> visitedCells;
-	private boolean visited = false;
 	
 	
 	public RecursiveBacktracker(int width, int height) {
-		mazeWidth = width;
-		mazeHeight = height;
-//		mazeGrid = new Cell[startX][startY];
-		stack = new Stack<StupidCell>();
-		visitedCells = new ArrayList<StupidCell>();
-		generateMaze(mazeWidth, mazeHeight);
+		this.width = width;
+		this.height = height;
+		initCells(width * height);
+		initPathCells(width * height);
+		initVisited(width * height);
+		generateMaze();
 	}
 	
-	private void generateMaze(int width, int height) {
-		StupidCell currentCell = new StupidCell(0, 0);
-		StupidCell newCell = new StupidCell();		
+	private void generateMaze() {
+		rnd = new Random();		
+		stack = new Stack<Cell>();		
 		
-		stack.push(currentCell); // add first cell to stack
-		ArrayList<StupidCell> neighbours = new ArrayList<StupidCell>();
+		Cell cell = getCellIndex(0);
+		stack.push(cell);
 		
 		while(!stack.isEmpty()) {
-			neighbours = checkPossibleNeighbours(currentCell, neighbours);
+			cell = stack.pop();
+			visited.set(cell.getIndex(), true);
+			cell.setOutNodes(getNeighbours(cell));
+			updateCell(cell);
 			
-			if(neighbours.size() > 0) {
-				Collections.shuffle(neighbours);
-				newCell = neighbours.get(0);  // get random neighbour
-				stack.add(newCell);
-				currentCell = newCell;				
-			} else {
-				currentCell = stack.pop();
-			} // if else
-		} // while
-	}
-	
-	private ArrayList<StupidCell> checkPossibleNeighbours(StupidCell cell, ArrayList<StupidCell> neighbours) {
-		
-		//North
-        if (cell.getY()-1 >= 0) {
-            StupidCell tmpCell = new StupidCell();
-            tmpCell.setX(cell.getX());
-            tmpCell.setY(cell.getY()-1);
-            if (!visitedCells.contains(tmpCell)){
-                neighbours.add(tmpCell);
-            }
-        }
-        //South
-        if (cell.getY()+1 < mazeHeight) {
-            StupidCell tmpCell = new StupidCell();
-            tmpCell.setX(cell.getX());
-            tmpCell.setY(cell.getY()+1);
-            if (!visitedCells.contains(tmpCell)){
-                neighbours.add(tmpCell);
-            }
-        }
-        //East
-        if (cell.getX()+1 < mazeWidth) {
-            StupidCell tmpCell = new StupidCell();
-            tmpCell.setX(cell.getX()+1);
-            tmpCell.setY(cell.getY());
-            if (!visitedCells.contains(tmpCell)){
-                neighbours.add(tmpCell);
-            }
-        }
-        //West
-        if (cell.getX()-1 >= 0) {
-            StupidCell tmpCell = new StupidCell();
-            tmpCell.setX(cell.getX()-1);
-            tmpCell.setY(cell.getY());
-            if (!visitedCells.contains(tmpCell)){
-                neighbours.add(tmpCell);
-            }
-        }
-		
-        return neighbours;
-	}
-	
-	private StupidCell getRandomCell(int width, int height) {
-		StupidCell rndCell = new StupidCell(rnd.nextInt(width), rnd.nextInt(height));
-		return rndCell;
-	}
-	
-	/*
-	public void traverseGrids(int width, int height) {
-		int row = height;
-		int col = width;
-		
-		for (int i = 0; i < row; i++) {
-			for (int j = 0; j < col; j++) {
-				mazeGrid[i][j] = new Cell(i, j);
+			if(cell.getCellOut().size() > 0) {
+				ArrayList<Cell> neighbours = cell.getCellOut();
+				Cell rndNeighbour = neighbours.get(rnd.nextInt(neighbours.size()));
+				cell.removeFromNodes(rndNeighbour);
+				updateCell(cell);
+				
+				Cell newCell = getCell2Index(cell.getIndex());
+				newCell.addToNodes(rndNeighbour);
+				updateCell2(newCell);
+				
+				Cell rndNewCell = getCell2Index(rndNeighbour.getIndex());
+				rndNewCell.addToNodes(newCell);
+				updateCell2(rndNewCell);
+				
+				stack.push(cell);
+				stack.push(rndNeighbour);
 			}
 		}
+	}
+	
+	private ArrayList<Cell> getNeighbours(Cell cell) {
+		ArrayList<Cell> neighbours = new ArrayList<Cell>();
+		int index = cell.getIndex();
+		int size = width * height;
 		
-	}*/
+		if(index >= width && !visited.get(index - width)) // North
+			neighbours.add(getCellIndex(index - width));
+		if(index < size - width && !visited.get(index + width)) // South
+			neighbours.add(getCellIndex(index + width));
+		if(index % width != 0 && !visited.get(index - 1)) // East
+			neighbours.add(getCellIndex(index - 1));
+		if(index % width != (width - 1) && !visited.get(index + 1)) // West // kontrollera arg..
+			neighbours.add(getCellIndex(index + 1));
+		
+		return neighbours;
+	}
+	
+	private Cell getCellIndex(int index) {
+        return cells.get(index);
+    }
+	
+	private Cell getCell2Index(int index) {
+        return paths.get(index);
+    }
+	
+	private void updateCell(Cell newCell) {
+		cells.set(newCell.getIndex(), newCell);
+	}
+	
+	private void updateCell2(Cell newCell) {
+		paths.set(newCell.getIndex(), newCell);
+	}
+	
+	private void initCells(int nbrOfCells) {
+		cells = new ArrayList<Cell>();
+        for (int i = 0; i < nbrOfCells; i++) {
+            cells.add(new Cell(i));
+        }
+	}
+	
+	private void initPathCells(int nbrOfCells) {
+		paths = new ArrayList<Cell>();
+        for (int i = 0; i < nbrOfCells; i++) {
+            paths.add(new Cell(i));
+        }
+	}
+	
+	private void initVisited(int nbrOfCells) {
+		visited = new ArrayList<Boolean>();
+        for (int i = 0; i < nbrOfCells; i++) {
+            visited.add(false);
+        }
+	}
+	
+	public void printMaze() {
+        System.out.print(" ");
+        for (int i = 0; i < width; i++) {
+            System.out.print("_ ");
+        }
+        System.out.println("");
+        for (int i = 0; i < (width * height); i++) {
+            Cell currentCell = paths.get(i);
+            if (isLeftEdge(currentCell)) {
+                System.out.print("|");
+            }
+            System.out.print(hasDown(currentCell) ? " " : "_");
+            System.out.print(hasRight(currentCell) ? (!isRightEdge(currentCell) ? " " : "") : "|");
+            if(i % width == width - 1) {
+                System.out.println("");
+            }
+        }
+	}
+	
+	private boolean isLeftEdge(Cell cell) {
+        return (cell.getIndex() % width == 0);
+    }
+
+    private boolean isRightEdge(Cell cell) {
+        return (cell.getIndex() % width == width - 1);
+    }
+
+    private boolean hasUp(Cell cell) {
+        int upIndex = cell.getIndex() - width;
+        return hasPathToIndex(cell, upIndex);
+    }
+
+    private boolean hasLeft(Cell cell) {
+        int leftIndex = cell.getIndex() - 1;
+        return hasPathToIndex(cell, leftIndex);
+    }
+
+    private boolean hasRight(Cell cell) {
+        int rightIndex = cell.getIndex() + 1;
+        return hasPathToIndex(cell, rightIndex);
+    }
+
+    private boolean hasDown(Cell cell) {
+        int downIndex = cell.getIndex() + width;
+        return hasPathToIndex(cell, downIndex);
+    }
+
+    private boolean hasPathToIndex(Cell cell, int matchIndex) {
+        ArrayList<Cell> neighbours = cell.getCellOut();
+        for (Cell neighbour : neighbours) {
+            if (neighbour.getIndex() == matchIndex) {
+                return true;
+            }
+        }
+        return false;
+    }
+	
 }
