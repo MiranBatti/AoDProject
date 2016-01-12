bce40f772832233e8f801ff74751777a8a2fbd7e
MiranBatti <miran.batti@gmail.com>
2016-01-12 18:46:07 +0100

    Använder stack klassen från labben

--- src/builder/Test.java
+++ src/builder/Test.java
@@ -2,7 +2,7 @@ package builder;
 
 public class Test {
 	public static void main(String[] args) {
-		RecursiveBacktrackerNew rb = new RecursiveBacktrackerNew(3, 3);
+		RecursiveBacktracker rb = new RecursiveBacktracker(3, 3);
 		rb.printMaze();
 	}
 }
